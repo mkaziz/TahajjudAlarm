1c634c4e7e008a578c035bc7318d209277d1b0f1
khalid <kaziz@u.northwestern.edu>
2015-05-31 14:53:24 -0500

    remove extraneous code

--- www/js/Settings/settings-controller.js
+++ www/js/Settings/settings-controller.js
@@ -5,31 +5,11 @@ angular.module("TahajjudAlarm").controller('SettingsController', ['prayTimes', '
 	    "use strict";
 		
 	    var self = this;
-		var prayerTimes = new PrayTimes();
-		prayerTimes.setMethod("ISNA");
-		
-		var chicagoTimes = prayerTimes.getTimes(new Date(), [42.056338, -87.696397]);
-		
-		var fajrStr = chicagoTimes.fajr;
-		
-		var hours = parseInt(fajrStr.split(":")[0]);
-		var minutes = parseInt(fajrStr.split(":")[1]);
-		
-		var fajrMoment = new Moment();
-		fajrMoment.hour(hours);
-		fajrMoment.minute(minutes);
-		fajrMoment.second(0);
-		
+			
 		self.minutesBeforeFajr = 0;
 		self.calculationMethodList = AlarmService.getCalculationMethodList();
-		self.calculationMethod = AlarmService.getCalculationMethod(); 
+		self.calculationMethod = AlarmService.getCalculationMethod(); 	
 		
-		
-		if (fajrMoment.isBefore(new Moment()))
-		{
-			fajrMoment.day(fajrMoment.day + 1);
-		}
-				
 		self.onSubmit = function () {
 			AlarmService.setAlarmTime(self.minutesBeforeFajrInput);
 			AlarmService.setCalculationMethod(self.calculationMethod);
