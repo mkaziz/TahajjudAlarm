ddfd3ad2472b2dae118a1f3afecc8cabbac7ea2a
khalid <kaziz@u.northwestern.edu>
2015-06-10 23:41:50 -0500

    Getting location

--- www/js/Settings/location-controller.js
+++ www/js/Settings/location-controller.js
@@ -1,20 +1,21 @@
 ///* global angular */
 angular.module("TahajjudAlarm").controller('LocationController', ['prayTimes', '$scope', 
-	'AlarmService', '$ionicLoading', "$cordovaGeolocation",
-	function(PrayTimes, $scope, AlarmService, $ionicLoading, $cordovaGeolocation) {
+	'AlarmService', '$ionicLoading', "$cordovaGeolocation", "$ionicPlatform",
+	function(PrayTimes, $scope, AlarmService, $ionicLoading, $cordovaGeolocation, $ionicPlatform) {
 	    "use strict";
 		
 	    var self = this;
-		
-		var posOptions = {timeout: 10000, enableHighAccuracy: false};
-		$cordovaGeolocation
-		    .getCurrentPosition(posOptions)
-		    .then(function (position) {
-				console.log(position);
-		    }, function(err) {
-	  			console.log(err);
-		    });
-
-		
+		self.searchForLocation = function () {
+			var posOptions = {timeout: 10000, enableHighAccuracy: false};
+			$cordovaGeolocation
+			    .getCurrentPosition(posOptions)
+			    .then(function (position) {
+					self.latitude = position.coords.latitude;
+					self.longitude = position.coords.longitude;
+					console.log(position);
+			    }, function(err) {
+		  			console.log(err);
+		    	});
+		};
 	}
 ]);
