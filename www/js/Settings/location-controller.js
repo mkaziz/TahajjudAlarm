dfdd8836127e7d3bb866dbb38b38fe9b5dcf60e5
khalid <kaziz@u.northwestern.edu>
2015-06-10 20:52:37 -0500

    Added ngCordova and geolocation

--- www/js/Settings/location-controller.js
+++ www/js/Settings/location-controller.js
@@ -1,10 +1,20 @@
 ///* global angular */
 angular.module("TahajjudAlarm").controller('LocationController', ['prayTimes', '$scope', 
-	'AlarmService', '$ionicLoading',
-	function(PrayTimes, $scope, AlarmService, $ionicLoading) {
+	'AlarmService', '$ionicLoading', "$cordovaGeolocation",
+	function(PrayTimes, $scope, AlarmService, $ionicLoading, $cordovaGeolocation) {
 	    "use strict";
 		
 	    var self = this;
+		
+		var posOptions = {timeout: 10000, enableHighAccuracy: false};
+		$cordovaGeolocation
+		    .getCurrentPosition(posOptions)
+		    .then(function (position) {
+				console.log(position);
+		    }, function(err) {
+	  			console.log(err);
+		    });
+
 		
 	}
 ]);
